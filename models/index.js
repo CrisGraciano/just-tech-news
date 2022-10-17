9be250b1ddd2caea4d2d3b785d592e50d558ce37
CrisGraciano <cristobalgraciano98@gmail.com>
2022-10-17 01:28:05 -0500

    post model created

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+module.exports = { User, Post };
 
 // this file is responsible is for importing the user model and exporting an object with it as a property
